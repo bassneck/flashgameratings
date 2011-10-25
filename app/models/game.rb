76ae48751919413f48e9265c25fe53c84b7a46ed
bassneck <bassneck@gmail.com>
2011-10-25 21:52:47 +0400

    if current_user is null, get requests with IN (NULL)

--- app/models/game.rb
+++ app/models/game.rb
@@ -7,5 +7,5 @@ class Game < ActiveRecord::Base
 	default_scope order("games.updated_at DESC")
 
 	scope :latest, where("games.updated_at > ?", Time.now - 2.weeks)
-	scope :unvoted, lambda{ |u| includes(:requests, :user).where("requests.id NOT IN (?)", u ? u.voted_request_ids : "") }
+	scope :unvoted, lambda{ |u| includes(:requests, :user).where("requests.id NOT IN (?)", u ? u.voted_request_ids : nil) }
 end
