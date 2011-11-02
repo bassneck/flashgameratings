d02a4b32948c5354e9dc484860e9ec795e8f7cab
bassneck <bassneck@gmail.com>
2011-11-02 20:44:44 +0400

    links can now be visited more than once. Vote is created on the first visit only

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -7,17 +7,11 @@ class RequestsController < ApplicationController
 	def show
 		request = Request.find(params[:id])
 
-		if request.game.user != current_user
-			if not current_user.voted_requests.exists?(request)
-				current_user.user_votes.create(:request => request)
-				redirect_to "http://#{request.url}"
-			else
-				flash[:error] = "Вы не можете голосовать дважды"
-				redirect_to root_url
-			end
-		else
-		    flash[:warning] = "Вы не можете голосовать за свои игры"
-			redirect_to root_url
+		if logged_in? and request.game.user != current_user and not current_user.voted_requests.exists?(request)
+			current_user.user_votes.create(:request => request)
 		end
+
+		redirect_to "http://#{request.url}"
+
 	end
 end
