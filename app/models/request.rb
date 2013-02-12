550fa0b143831a2e89d5356710669d34f7ce685c
Constantine Zaytsev <bassneck@gmail.com>
2013-02-12 17:45:59 +0400

    added portal_id to attr_accessible

--- app/models/request.rb
+++ app/models/request.rb
@@ -7,7 +7,7 @@ class Request < ActiveRecord::Base
 	has_many :user_votes
 	has_many :voters, class_name: 'User', through: :user_votes
 
-	attr_accessible :url, :portal
+	attr_accessible :url, :portal, :portal_id
 
 	validates_each :url do |model, attribute, value|
 		unless value =~ Regexp.new('^(http://|https://)?(www\.)?' + model.portal.url + model.portal.pattern + '$')
@@ -17,11 +17,7 @@ class Request < ActiveRecord::Base
 
 	after_create { |record| record.game.user.calculate_points.save }
 
-	scope :unvoted, lambda do |u|
-		if u
-			where('requests.id NOT IN (?)', u.voted_request_ids)
-		end
-	end
+	scope :unvoted, lambda { |u| where('requests.id NOT IN (?)', u.voted_request_ids) if u }
 
 	def url=(value)
 		unless value.blank?
