40ce0926902551da9635c0bb29f4f7f4d4bf3af4
Constantine Zaytsev <bassneck@gmail.com>
2013-02-18 00:59:11 +0400

    Appended html_safe to the analytics helper. 1 months of analytics lost. Sigh

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,8 @@ module ApplicationHelper
 
 	def analytics
 		if ENV['GOOGLE_ANALYTICS']
-			content_tag(:script, type: 'text/javascript') do
-				"
-				var _gaq = _gaq || [];
+			content_tag(:script, { type: 'text/javascript' }) do
+				"var _gaq = _gaq || [];
 				_gaq.push(['_setAccount', '#{ENV['GOOGLE_ANALYTICS']}']);
 				_gaq.push(['_trackPageview']);
 
@@ -12,8 +11,7 @@ module ApplicationHelper
 					var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
 					ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 					var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-				})();
-				"
+				})();".html_safe
 			end
 		else
 
