47e66dcdd5a4032214554301c3b68cc610ad6f14
Constantine Zaytsev <bassneck@gmail.com>
2019-08-10 20:31:33 +1200

    Extract secret token into an env variable

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Fgr::Application.config.secret_token = '2fc7a9d57a3db67a2a913eae2c19d5d62d07f15e4703db7cb3b8de4996b24414a9355c8090f8c63c861d70e4266531333deeb5cf74b2a66119a6876a6255af33'
+Fgr::Application.config.secret_token = ENV['SECRET_TOKEN']
